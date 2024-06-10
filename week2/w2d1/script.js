3d7eea63134737061442b5b9e07ac7b9fe4f4740
Devarsh Thaker <devarsht@gmail.com>
2024-06-10 13:49:22 -0700

    DOM, eventListner and getElementById

--- week2/w2d1/script.js
+++ week2/w2d1/script.js
@@ -1,5 +1,19 @@
-console.log("Here in the script.js");
+// Select the button elements
+const button1 = document.getElementById("button1");
+const button2 = document.getElementById("button2");
 
-//Select the cafe name
-const cafeName = document.getElementById("cafe-name");
-console.log("Cafe Name: ", cafeName.textContent);
+// Define the function to be executed when button 1 is clicked
+function showMessage1() {
+  const messageElement = document.getElementById("message");
+  messageElement.innerText = "Help me";
+}
+
+// Define the function to be executed when button 2 is clicked
+let showMessage2 = () => {
+  const messageElement = document.getElementById("message");
+  messageElement.innerText = "Aooga";
+};
+
+// Register the event listeners for the buttons
+button1.addEventListener("click", showMessage1);
+button2.addEventListener("click", showMessage2);
