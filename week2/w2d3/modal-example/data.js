e0d45bf690d894a43ae51f57c107109f9a6c6f91
Devarsh Thaker <devarsht@gmail.com>
2024-06-11 18:58:30 -0700

    inital setup for JS, DOM, and Modal video

---
+++ week2/w2d3/modal-example/data.js
@@ -0,0 +1,32 @@
+let playlistsData = {
+  playlists: [
+    {
+      cover: "image1.jpeg",
+      name: "Chill Vibes",
+      creator: "Alice",
+      likes: 42,
+      songs: [
+        { title: "Song A", artist: "Artist 1", duration: "3:45" },
+        { title: "Song B", artist: "Artist 2", duration: "4:15" },
+        { title: "Song C", artist: "Artist 3", duration: "2:55" },
+        { title: "Song D", artist: "Artist 4", duration: "3:30" },
+        { title: "Song E", artist: "Artist 5", duration: "1:55" },
+        { title: "Song F", artist: "Artist 46", duration: "13:30" },
+      ],
+    },
+    {
+      cover: "image2.jpeg",
+      name: "Workout Hits",
+      creator: "Bob",
+      likes: 58,
+      songs: [
+        { title: "Song A", artist: "Artist 1", duration: "3:45" },
+        { title: "Song B", artist: "Artist 2", duration: "4:15" },
+        { title: "Song C", artist: "Artist 3", duration: "2:55" },
+        { title: "Song D", artist: "Artist 4", duration: "3:30" },
+        { title: "Song E", artist: "Artist 5", duration: "1:55" },
+        { title: "Song F", artist: "Artist 46", duration: "13:30" },
+      ],
+    },
+  ],
+};
