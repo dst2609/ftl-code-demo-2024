80241dc8e8d55ebf403fadc5225132e25ea4bfe5
Devarsh Thaker <devarsht@gmail.com>
2024-06-13 10:49:46 -0700

    first react app - intro

---
+++ week2/w2d4-ReactIntro/first-react-app/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+
+// Skipping DOM import
+
+const App = () => {
+  return (
+    <div>
+      <h1>App Component</h1>
+    </div>
+  );
+};
+
+export default App;
