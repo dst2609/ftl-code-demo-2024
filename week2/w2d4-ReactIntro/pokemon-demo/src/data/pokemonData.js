51a090a9ab572b842002569e49cf754169ccb117
Devarsh Thaker <devarsht@gmail.com>
2024-06-13 21:27:20 -0700

    added pokemon images and fixed image css for cards

--- week2/w2d4-ReactIntro/pokemon-demo/src/data/pokemonData.js
+++ week2/w2d4-ReactIntro/pokemon-demo/src/data/pokemonData.js
@@ -5,8 +5,7 @@ const pokemonData = [
     type: "Grass/Poison",
     description:
       "There is a plant seed on its back right from the day this Pokémon is born. The seed slowly grows larger.",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2018/04/13/16/13/pixel-3316924_1280.png",
+    imageUrl: "https://img.pokemondb.net/artwork/large/bulbasaur.jpg",
   },
   {
     id: 2,
@@ -14,7 +13,7 @@ const pokemonData = [
     type: "Fire",
     description:
       "It has a preference for hot things. When it rains, steam is said to spout from the tip of its tail.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/charmander.jpg",
   },
   {
     id: 3,
@@ -22,7 +21,7 @@ const pokemonData = [
     type: "Water",
     description:
       "When it retracts its long neck into its shell, it squirts out water with vigorous force.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/squirtle.jpg",
   },
   {
     id: 4,
@@ -30,8 +29,7 @@ const pokemonData = [
     type: "Electric",
     description:
       "Pikachu that can generate powerful electricity have cheek sacs that are extra soft and super stretchy.",
-    imageUrl:
-      "https://cdn.pixabay.com/photo/2020/08/29/16/08/pikachu-5527377_1280.jpg",
+    imageUrl: "https://img.pokemondb.net/artwork/large/pikachu.jpg",
   },
   {
     id: 5,
@@ -39,7 +37,7 @@ const pokemonData = [
     type: "Fairy",
     description:
       "Jigglypuff's vocal cords can freely adjust the wavelength of its voice. This Pokémon uses this ability to sing at precisely the right wavelength to make its foes most drowsy.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/jigglypuff.jpg",
   },
   {
     id: 6,
@@ -47,7 +45,7 @@ const pokemonData = [
     type: "Normal",
     description:
       "It loves to collect shiny things. If it's in a good mood, it might even let its Trainer play with its hoard of treasures.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/meowth.jpg",
   },
   {
     id: 7,
@@ -55,7 +53,7 @@ const pokemonData = [
     type: "Water",
     description:
       "It is constantly wracked by a headache. When the headache turns intense, it begins using mysterious powers.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/psyduck.jpg",
   },
   {
     id: 8,
@@ -63,7 +61,7 @@ const pokemonData = [
     type: "Fighting",
     description:
       "It loves working out and builds its muscles by carrying other Pokémon as weights.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/machop.jpg",
   },
   {
     id: 9,
@@ -71,7 +69,7 @@ const pokemonData = [
     type: "Ghost/Poison",
     description:
       "On the night of a full moon, if shadows move on their own and laugh, it must be Gengar’s doing.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/gengar.jpg",
   },
   {
     id: 10,
@@ -79,7 +77,7 @@ const pokemonData = [
     type: "Normal",
     description:
       "Thanks to its unstable genetic makeup, this special Pokémon conceals many different possible evolutions.",
-    imageUrl: "",
+    imageUrl: "https://img.pokemondb.net/artwork/large/eevee.jpg",
   },
 ];
 
