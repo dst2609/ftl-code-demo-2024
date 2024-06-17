64f89e48bdcc696dcc9911d06a442a3f5f697c83
Devarsh Thaker <devarsht@gmail.com>
2024-06-17 10:31:38 -0700

    Added search

--- week3/w3d1/pokemon-modal/src/Components/PokemonList/PokemonList.jsx
+++ week3/w3d1/pokemon-modal/src/Components/PokemonList/PokemonList.jsx
@@ -3,11 +3,13 @@ import "./PokemonList.css";
 
 function PokemonList() {
   const [pokemons, setPokemons] = useState([]);
+  // const [selectedPokemon, setSelectedPokemon] = useState(null);
+  const [searchTerm, setSearchTerm] = useState("");
 
   useEffect(() => {
     async function fetchPokemon() {
       const response = await fetch(
-        "https://pokeapi.co/api/v2/pokemon?limit=20"
+        "https://pokeapi.co/api/v2/pokemon?limit=200"
       );
       const data = await response.json();
       setPokemons(data.results);
@@ -16,19 +18,34 @@ function PokemonList() {
     fetchPokemon();
   }, []);
 
+  // Filter Pokémon based on search term
+  const filteredPokemons = pokemons.filter((pokemon) =>
+    pokemon.name.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
   return (
-    <div className="pokemon-list">
-      {pokemons.map((pokemon) => (
-        <div className="pokemon-item" key={pokemon.name}>
-          <img
-            src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}
-            alt={pokemon.name}
-          />
-          <h4>{pokemon.name}</h4>
-          {/* <p>Type: {pokemon.type}</p> */}
-        </div>
-      ))}
-    </div>
+    <>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Search Pokémon..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          className="search-input"
+        />
+      </div>
+      <div className="pokemon-list">
+        {filteredPokemons.map((pokemon) => (
+          <div className="pokemon-item" key={pokemon.name}>
+            <img
+              src={`https://img.pokemondb.net/artwork/large/${pokemon.name}.jpg`}
+              alt={pokemon.name}
+            />
+            <h4>{pokemon.name}</h4>
+          </div>
+        ))}
+      </div>
+    </>
   );
 }
 
