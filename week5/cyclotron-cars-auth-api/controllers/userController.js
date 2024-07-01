9d683ab5241355b5768debe52ffae1d96a0fb096
Devarsh Thaker <devarsht@gmail.com>
2024-07-01 11:51:47 -0700

    login api endpoint created

--- week5/cyclotron-cars-auth-api/controllers/userController.js
+++ week5/cyclotron-cars-auth-api/controllers/userController.js
@@ -9,11 +9,13 @@ const {
 // REgister User
 const register = async (req, res) => {
   const { username, password } = req.body;
+  //   console.log("username", username, "password", password);
   try {
     // hash the password using bcrypt and salt factor 10
     const hashedPassword = await bcrypt.hash(password, 10);
     // user model to be saved using the hashedpassword
     const user = await createUser(username, hashedPassword);
+    res.status(201).json(user);
   } catch (error) {
     res
       .status(400)
@@ -22,6 +24,22 @@ const register = async (req, res) => {
 };
 
 // Login User
+// user exists?
+// password correct?
+const login = async (req, res) => {
+  const { username, password } = req.body;
+  const user = await findUserByUsername(username);
+  if (user && (await bcrypt.compare(password, user.password))) {
+    // create a json web token
+    const token = jwt.sign(
+      { userId: user.id, userName: user.username },
+      "SECRET KEY"
+    );
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ error: "Invalid Credentials" });
+  }
+};
 
 //export:
-module.exports = { register };
+module.exports = { register, login };
--- week5/cyclotron-cars-auth-api/routes/userRoutes.js
+++ week5/cyclotron-cars-auth-api/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const { register } = require("../controllers/userController");
+const { register, login } = require("../controllers/userController");
 
 const router = express.Router();
 
 router.post("/register", register);
+router.post("/login", login);
 
 module.exports = router;
